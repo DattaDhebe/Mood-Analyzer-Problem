0b7b3503fbd0e8f8acbc913762621fff927f2230
DattaDhebe <dattadhebe75@gmail.com>
2020-04-30 11:23:11 +0000

    Refactor. rename test case function

--- src/test/java/com/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/moodanalyzer/MoodAnalyzerTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
     @Test
-    public void givenAnalyseMood_ShouldReturnSAD(){
+    public void givenAnalyseMoodFunction_ShouldReturnSAD(){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("This is a Sad Message");
         Assert.assertEquals("SAD", mood);
     }
 
     @Test
-    public void givenAnalyseMood_SouldReturnHAPPY(){
+    public void givenAnalyseMoodFunction_SouldReturnHAPPY(){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String result = moodAnalyser.analyseMood("I am in Any Mood");
         Assert.assertEquals("HAPPY", result);
