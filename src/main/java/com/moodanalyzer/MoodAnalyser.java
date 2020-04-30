c236ed24cc63d35abf8d8f1d0c3af6172b75b2ed
DattaDhebe <dattadhebe75@gmail.com>
2020-04-30 11:44:32 +0000

    Refactor. added full message to return HAPPY or SAD

--- src/main/java/com/moodanalyzer/MoodAnalyser.java
+++ src/main/java/com/moodanalyzer/MoodAnalyser.java
@@ -3,7 +3,7 @@ package com.moodanalyzer;
 public class MoodAnalyser {
     public String analyseMood(String message) {
 
-        if ( message.contains("Any Mood") )
+        if ( message.contains("I am Any Mood") )
             return "HAPPY";
         else
             return "SAD";
